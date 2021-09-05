686217f90ec2221ad4b79aa303797d0b9c5b66cc
william <william.steinkedemello@gmail.com>
2021-09-05 20:45:27 +0200

    Staging changes in the first folder class

--- 1-First-Class/index.js
+++ 1-First-Class/index.js
@@ -1,8 +1,47 @@
 const http = require("http");
+const fs = require("fs");
 
 http.createServer((request, response) => {
     const { headers, method, url } = request;
     let body = [];
+
+    if (url === "/") {
+        //response.write(JSON.stringify(responseBody));
+
+        response.write(`
+                        <html>
+                            <head>
+                            <title>[YOUR_TITLE]</title>
+                            </head>
+                            <body>
+                                <form action='/message' method='POST'>
+                                    <input type='text' name='message' />
+                                    <button type='submit'>Send</button>
+                                </form>
+                            </body>
+                        </html>
+                `);
+        return response.end();
+    }
+
+    if (url === "/message" && method === "POST") {
+        fs.writeFileSync("message.txt", "Abacate");
+        response.statusCode = 302;
+        response.setHeader("Location", "/");
+        return response.end();
+    }
+    response.write(`
+                        <html>
+                            <head>
+                                <title>Hello Node!</title>
+                            </head>
+                            <body>
+                                <h1>Hello node!</h1>
+                            </body>
+                        </html>               
+                `);
+    response.end();
+
     request
         .on("error", (err) => {
             console.error(err);
@@ -18,15 +57,12 @@ http.createServer((request, response) => {
                 console.error(err);
             });
 
-            response.statusCode = 200;
-            response.setHeader("Content-Type", "application/json");
+            //response.setHeader("Content-Type", "application/json");
             // Note: the 2 lines above could be replaced with this next one:
             // response.writeHead(200, {'Content-Type': 'application/json'})
 
             const responseBody = { headers, method, url, body };
 
-            response.write(JSON.stringify(responseBody));
-            response.end();
             // Note: the 2 lines above could be replaced with this next one:
             // response.end(JSON.stringify(responseBody))
 
